1e73fe047f9b9c3c5ada4a9250b115f05bc3ea20
zetsub0 <g.echilov@gmail.com>
2024-01-25 16:27:13 +0300

    after ch3.3

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/zetsub0/greenlight/internal/data"
 	"net/http"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,5 +17,14 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
 }
