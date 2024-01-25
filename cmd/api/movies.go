d40037de379487885373903eca2a14893201a59d
zetsub0 <g.echilov@gmail.com>
2024-01-25 06:55:37 +0300

    after ch2.3

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParams(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zetsub0/greenlight
 
 go 1.21
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
