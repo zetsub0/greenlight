89d1ab8c127172fa0d00314687fc3b20409e2b23
zetsub0 <g.echilov@gmail.com>
2024-01-28 10:34:59 +0300

    after ch4.3

--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 func (app *application) readIDParams(r *http.Request) (int64, error) {
@@ -41,8 +42,13 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	MaxBytes := 1024 * 1024 // (2^10 * 2^10)bytes == 1MiB
+	r.Body = http.MaxBytesReader(w, r.Body, int64(MaxBytes))
 
-	err := json.NewDecoder(r.Body).Decode(dst)
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
 	if err != nil {
 		var (
 			syntaxError           *json.SyntaxError
@@ -63,9 +69,18 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 			return errors.New("body must not be empty")
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", MaxBytes)
 		default:
 			return err
 		}
+	}
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
 }
