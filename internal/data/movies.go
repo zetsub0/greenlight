d4f82a8b68221b8a581fd79ceb0f16fee8b55f76
zetsub0 <g.echilov@gmail.com>
2024-01-31 21:21:57 +0300

    after ch7.2

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"github.com/lib/pq"
 	"github.com/zetsub0/greenlight/internal/validator"
 	"time"
 )
@@ -38,8 +39,14 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
+	query := `
+		INSERT INTO movies (title, year, runtime, genres)
+		VALUES($1, $2, $3, $4)
+		RETURNING id, created_at, version`
 
-	return nil
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Ger(id int64) (*Movie, error) {
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -11,19 +11,19 @@ var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
-func (r *Runtime) MarshalJSON() ([]byte, error) {
+func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
 }
 
 func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
-	unquotedSJONValue, err := strconv.Unquote(string(jsonValue))
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
 	if err != nil {
 		return ErrInvalidRuntimeFormat
 	}
 
-	parts := strings.Split(unquotedSJONValue, " ")
+	parts := strings.Split(unquotedJSONValue, " ")
 	if len(parts) != 2 || parts[1] != "mins" {
 		return ErrInvalidRuntimeFormat
 	}
